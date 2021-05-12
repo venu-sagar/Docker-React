0de72b431453e26002525a8d9bab1584274dd85a
venu-sagar <sagar.venu@fenecon.de>
2021-05-12 18:23:35 +0200

    initial comment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, wassup!, I am Changed
         </p>
         <a
           className="App-link"
